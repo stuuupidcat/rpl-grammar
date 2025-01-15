8ce1b6567faed2f280c880a504986048a95be178
stuuupidcat <stuuupidcat@163.com>
2025-01-15 11:18:23 +0800

    Update

---
+++ tests/test.rs
@@ -0,0 +1,34 @@
+#[cfg(test)]
+use parser::{Grammar, pairs};
+
+macro_rules! full_test {
+    ($T:ident, $input:expr $(,)?) => {{
+        use pest_typed::TypedParser as _;
+        let input = $input;
+        match Grammar::try_parse::<pairs::$T>(input) {
+            Ok(_res) => {
+                // println!("{:#?}", _res);
+            }
+            Err(e) => {
+                eprintln!("Failed to parse input :\n{}", $input);
+                // eprintln!("{}", e);
+                panic!("\n{}\n", e);
+            }
+        }
+    }};
+}
+
+#[test]
+fn main() {
+    full_test!(
+        main,
+        "\
+pattern CVE-2018-21000
+
+patt {
+    _ = $A
+    _ = $B
+}
+"
+    )
+}
